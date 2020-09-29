985b88aed535dbf567fb521a41f704d091dd9446
Ashrrith Karunaakar <ashrrithkarunaakar@gmail.com>
2020-09-29 20:23:26 +0530

    test

--- app/src/main/java/in/christuniversity/cunavigator/StudentLogin.java
+++ app/src/main/java/in/christuniversity/cunavigator/StudentLogin.java
@@ -1,21 +1,94 @@
 package in.christuniversity.cunavigator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class StudentLogin extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-    public void login(View view){
-        Intent i = new Intent(StudentLogin.this, StudentDashboard.class);
-        startActivity(i);
-    }
+import java.util.regex.Pattern;
+
+public class StudentLogin extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+    private TextInputEditText stuEmail, stuPassword;
+   private ProgressBar login_progress_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
+
+        stuEmail = findViewById(R.id.edit_email);
+        stuPassword = findViewById(R.id.edit_password);
+        login_progress_bar = findViewById(R.id.login_progress_bar);
+
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.login).setOnClickListener(this);
+
+        login_progress_bar.setVisibility(View.INVISIBLE);
+
+    }
+
+    private void userLogin() {
+
+        String email = stuEmail.getText().toString().trim();
+        String password = stuPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            stuEmail.setError("Email is Required");
+            stuPassword.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            stuEmail.setError("Enter a valid email");
+            stuPassword.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()) {
+            stuEmail.setError("Password is Required");
+            stuPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 8) {
+            stuEmail.setError("Min length of password is 8");
+            stuPassword.requestFocus();
+            return;
+        }
+
+        login_progress_bar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent dashboard = new Intent(StudentLogin.this, StudentDashboard.class);
+                    dashboard.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(dashboard);
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+         userLogin();
     }
 }
